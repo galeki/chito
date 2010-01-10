31f84a8e3e43d1b9ee1376fd77b571124c7ec4f1
galeki <linuxgem.org@gmail.com>
2010-01-10 11:30:36 +0800

    fix 1.1.4 bugs

--- app/helpers/admin/comment_helper.rb
+++ app/helpers/admin/comment_helper.rb
@@ -2,11 +2,9 @@ module Admin::CommentHelper
 
     def show_filter(filter)
 	 %Q[<li class="filter" id="filter_#{filter.id}">
-		#{(link_to_ajax_dialog 'confirm', image_tag("setting.gif"),
-				      {:url => filter.form_url, :title => filter.name, :width => 300,  :okLabel => "Save",:cancelLabel => "Cancel", :className => "alphacube", 
-				       :draggable => true, :resizable => true, :closable => true, :destroyOnClose => true,
-				       :onOk => "function(win){ remote_form();return true;}"}, 
-				      {:class => "remote_setting"}) if filter.config} 
+		#{(link_to image_tag("setting.gif"), "#", 
+		    {:onclick => "open_remote_form({'url':'#{filter.form_url}', 'title':'#{filter.name}', 'width':450});", 
+		    :class => "remote_setting"} )  if filter.config} 
 		#{filter.name } 
 	    </li>]
     end
