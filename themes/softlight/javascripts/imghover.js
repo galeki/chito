0046b3280ad0997291b90dbb13477015237042f7
galeki <linuxgem.org@gmail.com>
2010-04-25 23:34:35 +0800

    add index theme module

--- themes/softlight/javascripts/imghover.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-function changeSty(classpassed){
-	element=event.srcElement;
-	//document.getElementById('buttonIMG').className=classpassed;
-	element.className=classpassed;
-}
