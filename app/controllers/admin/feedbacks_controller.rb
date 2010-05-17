d857f11067b1c681a1df0131b9f862731d091cc1
galeki <linuxgem.org@gmail.com>
2010-05-18 01:15:17 +0800

    fix admint/comments jump off

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -1,19 +1,22 @@
 class Admin::FeedbacksController < Admin::BaseController
-    before_filter :chito_admin_authorize    
+    #before_filter :chito_admin_authorize    
 
   def index
+    chito_admin_authorize
     @feedbacks = Feedback.paginate :per_page => 30, :page => params[:page], 
                                    :include => [:article, :user],
                                    :order => 'created_at desc'
   end
 
   def destroy
+    chito_admin_authorize
     @feedback = Feedback.find(params[:id])
     notice_stickie t(:feedback_deleted, :scope => [:txt, :controller, :admin, :feedbacks]) if @feedback.destroy
     redirect_to admin_feedbacks_path   
   end
    
   def destroy_selected
+        chito_admin_authorize
 	destroy_global_ids(params[:ids])
 	notice_stickie t(:feedbacks_deleted, :scope => [:txt, :controller, :admin, :feedbacks])
 	redirect_to(admin_feedbacks_path :keyword => params[:keyword], :page => params[:page])
