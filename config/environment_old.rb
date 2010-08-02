314338cdf4292a11982f7933fd6e24ada989f80a
galeki <linuxgem.org@gmail.com>
2010-08-02 14:35:50 +0800

    update rails projrct files

---
+++ config/environment_old.rb
@@ -0,0 +1,95 @@
+# Be sure to restart your web server when you modify this file.
+
+# Uncomment below to force Rails into production mode when 
+# you don't control web/app server and can't set it the proper way
+# ENV['RAILS_ENV'] ||= 'production'
+
+# Specifies gem version of Rails to use when vendor/rails is not present
+#$KCODE = 'u'
+#require 'jcode' 
+#RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+require "chito_helper"
+require "chito_version"
+Rails::Initializer.run do |config|
+  # Settings in config/environments/* take precedence those specified here
+  # Skip frameworks you're not going to use (only works if using vendor/rails)
+   #config.frameworks -= [ :action_mailer ]
+
+  #config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
+  #config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style', 'align', 'border', 'margin', 'padding'
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/extras )
+
+  # Force all environments to use the same logger level 
+  # (by default production uses :info, the others :debug)
+  # config.log_level = :debug
+
+  # Use the database for sessions instead of the file system
+  # (create the session table with 'rake db:sessions:create')
+  #
+   #config.action_controller.session_store = :p_store
+   #config.action_controller.session = { :key => "_chito_session_115"}
+
+   #config.action_controller.session_store = :active_record_store
+
+   config.action_controller.cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
+   #config.action_controller.fragment_cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
+   
+   
+   config.plugins = [ :chito_plugin, :all ]
+   #config.action_controller.session_store = :memory_store
+  config.load_paths += %W(
+    vendor/simple-rss/lib
+    vendor/uuidtools/lib
+  ).map {|dir| "#{RAILS_ROOT}/#{dir}"}.select { |dir| File.directory?(dir) } 
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper, 
+  # like if you have constraints or database-specific column types
+  # config.active_record.schema_format = :sql
+
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector
+
+  # Make Active Record use UTC-base instead of local time
+  # config.active_record.default_timezone = :utc
+  config.action_controller.session = {
+    :key => '_chito_session_117_beta_2',
+    :secret      => 'cfb8733302e2533ca5d22b4cb72af032a56634a69fac916401f6b1cc21284743a0149bbbadf30d576b'
+  } 
+  # See Rails::Configuration for more options
+
+  config.action_view.sanitized_allowed_tags = %w(u strong em b i p code pre sub sup cite small address br div span ul ol li dt dd abbr acronym img blockquote)
+  config.action_view.sanitized_allowed_attributes = %w(src alt cite title class style id)
+end
+#ActionController::Base.fragment_cache_store = ActionController::Caching::Fragments::FileStore.new
+
+Time::DATE_FORMATS[:db_short] = "%Y-%m-%d %H:%M"
+Time::DATE_FORMATS[:update] = "<b>%H:%M</b>"
+
+
+#ActionController::Base.session_options[:session_key] = "chito_session"
+   
+#require 'url_helper_patch'
+#Mime::Type.register "application/pdf", :pdf
+#require 'gettext/rails'
+#require 'will_paginate'
+#require 'pseudo_gettext'
+
+#ActionMailer::Base.raise_delivery_errors = false
+#ActionMailer::Base.default_charset = "utf-8"
+#ROOT_DOMAIN = 'the-programmer.net'  
+#ActionController::Base.session_options[:session_domain] = ROOT_DOMAIN  
+
+# Add new inflection rules using the following format 
+# (all these examples are active by default):
+# Inflector.inflections do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# Include your application configuration below
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '17fd94360191822e33c00049f988a170a9ed48e19c275297187d2fc046741dcb55fe99621e296444c7511d66e828005a878c497661064dc150c65d743b5b0e14'
