87600703d5544f97623d38d578fed4ec000d1178
galeki <linuxgem.org@gmail.com>
2010-08-02 14:17:47 +0800

    begin clean needless plugins

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-$KCODE = 'u'
-require 'jcode' 
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+#$KCODE = 'u'
+#require 'jcode' 
+#RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
