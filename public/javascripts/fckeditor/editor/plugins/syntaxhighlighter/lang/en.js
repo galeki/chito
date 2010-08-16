c89886ddccae1bf0cb95cfb0530ef574594942d2
galeki <linuxgem.org@gmail.com>
2010-08-16 21:36:21 +0800

    clean FCKeditor files and add smeliy to CKeditor

--- public/javascripts/fckeditor/editor/plugins/syntaxhighlighter/lang/en.js
+++
@@ -1,8 +0,0 @@
-FCKLang.SyntaxHighlighterTitle	= 'Syntax Highlighter';
-FCKLang.SyntaxHighlighterLang	= 'Language: ';
-FCKLang.SyntaxHighlighterCode	= 'Code';
-FCKLang.SyntaxHighlighterLine	= 'First line number: ';
-FCKLang.SyntaxHighlighterCodeTitle	= 'Code title: ';
-FCKLang.SyntaxHighlighterCollapse	= 'Collapse';
-FCKLang.SyntaxHighlighterTip1	= 'Dont forget to enable SyntaxHighlighter in Chito plugins settings.';
-FCKLang.SyntaxHighlighterTip2	= 'And the highlighted code will show when your post is loaded.';
