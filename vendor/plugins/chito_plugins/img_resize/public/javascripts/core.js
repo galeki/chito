40e050d055e57a7ee5566853b9471c05dd5e5a6c
galeki <linuxgem.org@gmail.com>
2010-08-18 17:09:09 +0800

    fix img_resize

--- vendor/plugins/chito_plugins/img_resize/public/javascripts/core.js
+++ vendor/plugins/chito_plugins/img_resize/public/javascripts/core.js
@@ -2,23 +2,26 @@ function img_resize_normal(w, text){
   for (i=0;i<document.images.length;i++)
    {
    if (document.all){
-	if (document.images[i].width>w)
+	if (document.images[i].width>w || document.images[i].style.width>w)
 	 {
-	    newHeight=document.images[i].height*w/document.images[i].width;
+	    var newHeight=document.images[i].height*w/document.images[i].width;
 	    document.images[i].width=w;
 	    document.images[i].height=newHeight;
+	    document.images[i].style.width=w;
+	    document.images[i].style.height=newHeight;  
        try{
 	       document.images[i].outerHTML='<a href="'+document.images[i].src+'" target="_blank" title="'+text+'">'+document.images[i].outerHTML+'</a>';
   	 	}catch(e){}
   	 }
    }
   else{
-	if (document.images[i].width>w) {
+	if (document.images[i].width>w || document.images[i].style.width>w) {
 	  document.images[i].title=text;
 	  document.images[i].style.cursor="pointer";
-	    newHeight=document.images[i].height*w/document.images[i].width;
+	    var newHeight=document.images[i].height*w/document.images[i].width;
 	    document.images[i].width=w;
-	    document.images[i].height=newHeight;  
+	    document.images[i].height=newHeight;
+            $(document.images[i]).css({'width': w, 'height':newHeight});
 	  document.images[i].onclick=function(e){window.open(this.src)};
 	}
   }
@@ -32,6 +35,7 @@ function img_resize_lightbox(w){
     {
 	var newh = myh * w / myw;
         $(this).attr({'width': w, 'height': newh});
+        $(this).css({'width': w, 'height': newh});
 
         $(this).wrap('<a href="' + $(this).attr('src') + '" class="lightbox"></a>');
     }
