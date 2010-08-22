fe26b01a63ec63f9af1e5bda04b0da5dea2b860d
galeki <linuxgem.org@gmail.com>
2010-08-23 01:53:13 +0800

    improve dashboardbar

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -2,9 +2,10 @@ module Admin::DashboardHelper
 
     def show_dashboardbar(bar, options={})
         content_tag :li, :id => "bar_#{bar.id}" do
+            bar_settings_area(bar).to_s +
             content_tag(:h2, title_of(bar)) +
             if options[:disable]
-                content_tag :div, "Refresh to see content", :class => "dashboardbar_content", :id => "bar_#{bar.id}_content"
+                content_tag :div, t(:save_to_see, :scope => [:txt, :view, :admin, :dashboard]), :class => "dashboardbar_content", :id => "bar_#{bar.id}_content"
             else
                 content_tag :div, content_of(bar), :class => "dashboardbar_content", :id => "bar_#{bar.id}_content"
             end
--- vendor/plugins/chito_plugins/useful_dashboardbars/lib/useful_dashboardbars.rb
+++ vendor/plugins/chito_plugins/useful_dashboardbars/lib/useful_dashboardbars.rb
@@ -2,20 +2,30 @@ module ApplicationPlugin
     private
     
     def find_official_notices_and_new_posts_before_admin_dashboard_show
-	@notices = []
-	of = User.find_by_name('official')
-	@notices = of.posts.order('created_at desc').limit(4) if of
+        if @user.show_dashboard_new_comments
+            @new_comments = @user.new_comments(@user.dashboard_new_comments_number.to_num(5))
+        end
+        if @user.show_dashboard_new_messages
+            @new_messages = @user.new_messages(@user.dashboard_new_messages_number.to_num(5))
+        end
+	#@notices = []
+	#of = User.find_by_name('official')
+	#@notices = of.posts.order('created_at desc').limit(4) if of
+        if @user.show_dashboard_posts_update
+	    num = @user.numbers_of_new_posts_in_admin.to_num(6)
+	    num = 30 if num > 50
+	    @new_posts = Article.new_posts(num)
+        end
 
-	num = @user.numbers_of_new_posts_in_admin.to_num(6)
-	num = 30 if num > 50
-	@new_posts = Article.new_posts(num)
-
-        @new_comments = @user.new_comments(4)	    
-	@new_messages = @user.new_messages(4)	    
-	@comments_size = @user.comments.size
-	@messages_size = @user.messages.size
-	@posts_size = @user.posts.size
-	@drafts_size = @user.drafts.size
+        if @user.show_dashboard_statistics
+	    @comments_size = @user.comments.size
+	    @messages_size = @user.messages.size
+	    @posts_size = @user.posts.size
+	    @drafts_size = @user.drafts.size
+        end
+        if @user.show_dashboard_links
+            @links = @user.links
+        end
     end
 end
 
@@ -68,7 +78,15 @@ bar = Dashboardbar.new
 bar.id = :dashboard_welcome
 bar.info = 'Welcome'
 bar.plugin_id = :useful_dashboardbars
-bar.config = true
+bar.config = false
 bar.default_position = 1
+Dashboardbar.add(bar)
+
+bar = Dashboardbar.new
+bar.id = :dashboard_links
+bar.info = 'Links'
+bar.plugin_id = :useful_dashboardbars
+bar.config = false
+bar.default_position = 6
 Dashboardbar.add(bar)
 
