7762e5942b66904a446b163a43be9d146de67324
galeki <linuxgem.org@gmail.com>
2010-08-28 00:47:32 +0800

    clean unneed files

--- db/perisistent.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists persistent_logins;
-create table persistent_logins (
-id	    int		not null auto_increment,
-uid	    varchar(100) not null,
-user_id	    int,
-created_on  datetime,
-primary key (id)
-)
