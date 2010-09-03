3754cfc0c6f8a439b22736af1ec9c66a313cb848
galeki <linuxgem.org@gmail.com>
2010-09-03 14:24:15 +0800

    fix setup problem

--- lib/chito_routes.rb
+++ lib/chito_routes.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class IndexMatch
     def self.matches?(request)
-        Site.instance.get_index(request)
+        Site.instance.get_index(request) if Site.instance
     end
 end
