5ab30c28e2d0647d843e650d44ae1e40f9d5bdae
galeki <linuxgem.org@gmail.com>
2010-10-02 18:23:56 +0800

    do not display captcha if user is logged in

--- vendor/plugins/chito_plugins/simple_vcode_comment_filter/lib/simple_vcode_comment_filter.rb
+++ vendor/plugins/chito_plugins/simple_vcode_comment_filter/lib/simple_vcode_comment_filter.rb
@@ -3,7 +3,7 @@ CommentFilter.add(filter)
 
 module BlogHelperPlugin
     def show_simple_vcode_field_in_comment_form
-        if @user.enable_comment_filter_simple_vcode
+        if @user.enable_comment_filter_simple_vcode && session[:user_name].nil?
             "<div id='show_simple_captcha'>" +
                 show_simple_captcha(:image_style => "simply_green", :label => t("txt.view.blog.add.input_captcha"), :distortion => "high") +
             "</div>"
@@ -18,9 +18,11 @@ module CommentControllerPlugin
   private
 
   def check_simple_vcode
-    @call = "re_captcha"
-    unless simple_captcha_valid?
-        block_filter(:type => :error, :message => t("plugins.simple_vcode_comment_filter.error_captcha"), :drop => true) #unless params[:code] == code
+    if session[:user_name].nil?
+        @call = "re_captcha"
+        unless simple_captcha_valid?
+            block_filter(:type => :error, :message => t("plugins.simple_vcode_comment_filter.error_captcha"), :drop => true)
+        end
     end
   end
 
