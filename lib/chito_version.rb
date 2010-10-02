30a72133b660f670fba2f76a04c0855f24ba21c5
galeki <linuxgem.org@gmail.com>
2010-10-02 18:03:09 +0800

    1.2.2 beta

--- lib/chito_version.rb
+++ lib/chito_version.rb
@@ -3,8 +3,8 @@ module Chito
     module VERSION
         MAJOR = 1
         MINOR = 2
-        TINY  = 1
-        BUILD = ""
+        TINY  = 2
+        BUILD = "beta"
 
         STRING = [MAJOR, MINOR, TINY, BUILD].join('.')
     end
