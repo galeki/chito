924a87ed77cb23b071256181e09a5bb4c76be2c5
galeki <linuxgem.org@gmail.com>
2010-10-20 00:01:01 +0800

    update jquery and jquery ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
 
     def jquery_include_tag
         if @site.use_external_jquery
-           javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"
+           javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"
         else
             javascript_include_tag "jquery"
         end
@@ -25,7 +25,7 @@ module ApplicationHelper
 
     def jquery_ui_include_tag
         if @site.use_external_jquery
-            javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.min.js"
+            javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js"
         else
             javascript_include_tag "jquery-ui"
         end
