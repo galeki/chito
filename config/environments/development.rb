4a69c86a9eb1718136c83ac6d8cb0c2d1f163a9d
galeki <linuxgem.org@gmail.com>
2011-12-20 22:30:46 +0800

    start move to Rails3.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Chito::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = true
 
   config.action_mailer.raise_delivery_errors = true
