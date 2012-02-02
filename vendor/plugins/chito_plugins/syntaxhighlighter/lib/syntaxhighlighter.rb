fabfa674808b48fcbb22149c86c67aae1bdaaf58
galeki <linuxgem.org@gmail.com>
2012-02-02 21:50:06 +0800

    move syntaxhighlighter css link to head, for easy custom css rewrite

--- vendor/plugins/chito_plugins/syntaxhighlighter/lib/syntaxhighlighter.rb
+++ vendor/plugins/chito_plugins/syntaxhighlighter/lib/syntaxhighlighter.rb
@@ -1,5 +1,15 @@
     module BlogHelperPlugin
-        def add_syntaxhighlighter_css_js_link_in_blog_tail
+        def add_syntaxhighlighter_css_link_in_head
+            head = ""
+            if @user.enable_syntaxhighlighter
+                theme = @user.syntaxhighlighter_theme || "Default"
+                head += plugin_stylesheet_link_tag('syntaxhighlighter', "shCore.css") + "\n"
+                head += plugin_stylesheet_link_tag('syntaxhighlighter', "shTheme#{theme}.css") + "\n"
+            end
+            return head.html_safe
+        end
+
+        def add_syntaxhighlighter_js_link_in_blog_tail
             head = ""
             if @user.enable_syntaxhighlighter
                 head = plugin_javascript_inculde_tag('syntaxhighlighter', "shCore.js") + "\n"
@@ -8,10 +18,6 @@
 
                 head += %Q!<script type="text/javascript">SyntaxHighlighter.all();</script>!.html_safe
 
-                head += plugin_stylesheet_link_tag('syntaxhighlighter', "shCore.css") + "\n"
-
-                theme = @user.syntaxhighlighter_theme || "Default"
-                head += plugin_stylesheet_link_tag('syntaxhighlighter', "shTheme#{theme}.css") + "\n"
             end
             return head.html_safe
         end
