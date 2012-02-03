7f19fff7c39b9c3df85e5c183fa6d3d5755e0058
galeki <linuxgem.org@gmail.com>
2012-02-04 02:48:31 +0800

    assets is good, but use it next time

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ end
 module Chito
   class Application < Rails::Application
     # Enable the asset pipeline
-    config.assets.enabled = true
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
