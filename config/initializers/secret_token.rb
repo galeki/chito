aaf23d84cd9f858397e476336708f634dd385ea4
galeki <linuxgem.org@gmail.com>
2012-02-18 00:33:54 +0800

    update:configs

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Chito::Application.config.secret_token = '17fd94360191822e33c00049f988a170a9ed48e19c275297187d2fc046741dcb55fe99621e296444c7511d66e828005a878c497661064dc150c65d743b5b0e14'
+Chito::Application.config.secret_token = 'bfcfed3e8fd6d31263f2021cd0f77766d939c5a2a7dd934eb4157bfe546d2fa1c097586394527c2c8d88f2f28aba47ae785882a8586ec8671474156c58d41e0b'
